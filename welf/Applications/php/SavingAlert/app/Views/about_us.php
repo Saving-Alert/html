c833109d5073a6d19fa072fb81ac76da9eddda7a
akeela-azhar <akeelaazhar13@gmail.com>
2024-05-29 01:33:57 +0530

    Add: Views About Us page

---
+++ welf/Applications/php/SavingAlert/app/Views/about_us.php
@@ -0,0 +1,35 @@
+<section class="about-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 offset-lg-3">
+					<div class="main-title text-center">
+						<h2 class="mt0">About Us</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-8 col-xl-7">
+					<div class="about_content">
+						<!-- <p class="large">WelfareAro, a web based non-profit food donation application is mainly uses for helping the people who are in hungry and acts as a bridge between the donors and the receivers..</p>
+
+						<p>The main purpose of this project is to provide an online platform for the organizations, donors and receivers who is in food donation and receiving, to save time of the food donors, easy access for hunger people, improve food donations and to make it more comfortable for the food donors and receivers.</p>
+
+						<p>There is no such existing system for food donations in Sri Lanka, individuals and organizations are individually contact receivers by visiting or over the phone if they could find the number. Most of the time donors find receivers by using social media like Facebook or word of mouth. Most of the time donors donate to elderly care homes or children's homes in city area. But there are thousands of people in rural areas and even in urban areas who is suffering hunger without any help. Also there are thousands of people, who is willing to donate food but they couldn't find receivers. </p>
+						
+						<p>WelfareAro food donation web application is mainly towards persons who are willing to donate food or persons who are willing to receive food. Through this web application it will be easier to find a donor or receiver and easy to build the connection between donor and receivers. The main intend of building this web application is to formal the procedure of food donation. The system also consists of manual post and user approve process, donor reviews and blacklisted user list as well. The entire web application includes the all relevant features to create a better connection between the donor and receivers.</p>
+						 -->
+					</div>
+				</div>
+				<div class="col-lg-4 col-xl-5">
+					<div class="about_thumb">
+						<!-- <img class="img-fluid w100" src="images/about/1.jpg" alt="1.jpg"> -->
+						
+					</div>
+				</div>
+
+			</div>
+
+			
+			
+		</div>
+	</section>
