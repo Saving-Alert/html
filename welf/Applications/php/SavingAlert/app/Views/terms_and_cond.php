a31aaf67c28942e7f5e55cc3e09786e1147f5fb4
Thilina <prem.tsan@gmail.com>
2024-05-29 17:06:56 +0530

    Add: Terms and conditions page for views

---
+++ welf/Applications/php/SavingAlert/app/Views/terms_and_cond.php
@@ -0,0 +1,79 @@
+<section class="about-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+                <div class="main-title text-center">
+                    <h2 class="mt0">Terms and Conditions</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-xl-7">
+                <div class="about_content">
+
+                    <p>
+
+
+                        Welcome to the user agreement (the "Agreement" or "User Agreement") for WelfareAro. This Agreement describes the terms and conditions applicable to your use of our products and services. If you do not agree to be bound by the terms and conditions of this Agreement, you are not permitted to access our website, resources, or services. WelfareAro may be used interchangeably throughout this User Agreement.  The terms "we", "ours", or "us" may be used interchangeably throughout this agreement and refer to WelfareAro.  You must read, agree with and accept all of the terms and conditions contained in this User Agreement and the Privacy Policy, which include those terms and conditions expressly set out below and those incorporated by reference, before you may become a member of WelfareAro.
+
+                        <br><br>
+
+                        We may amend this Agreement at any time by posting the amended terms on the Site. Except as stated below, all amended terms shall automatically be effective 30 days after they are initially posted on the Site.
+
+                        <br><br>
+
+                        Copyrighted Materials: <br><br>
+
+                        This web site, and materials contained within, are subject to the protection of USA and International copyright laws. Users may not reproduce any of these materials without the prior written permission of WelfareAro.
+
+                        <br><br>
+
+                        Liability: <br><br>
+                        In no event shall WelfareAro be liable for any punitive, special, direct, indirect, incidental, or consequential damages, whether arising under contract, warranty, or tort (including negligence or strict liability) or any other theory of liability. Applicable law may not allow the limitation or exclusion of liability or incidental or consequential damages, so the above limitation or exclusion may not apply to you. WelfareAro's liability for damages, regardless of the form of the action, shall not exceed the fee paid for the service. We cannot be held responsible for factors beyond our control that may interfere with our ability to provide access to our website. Such factors include, but are not limited to server downtime, password/verification problems, or network outages beyond our servers. WelfareAro does not warrant that the functions contained in the materials, resources or services offered will be uninterrupted or error-free, that defects will be corrected, or that this site are free of viruses or other harmful elements. In no event will WelfareAro be liable to any party for any direct, indirect, punitive, special, incidental or other consequential damages arising directly or indirectly from any use of the products or resources contained within the WelfareAro website. All products and resources are provided "as is" and without warranties.
+
+                        <br><br>
+
+
+                        Termination: <br><br>
+                        These Terms of Use are effective until terminated by either party. You may terminate these Terms of Use by destroying all copies and installations of products and services obtained from us. The agreement will automatically and immediately terminate without notice should you violate any terms of this agreement. Violating our Terms Of Use, Copyrights, or any of our licensing restrictions, will result in termination of membership, and forfeiture of all reseller licenses, without notice or refund.
+
+                        <br><br>
+
+                        Users agree to comply with all applicable domestic and international laws, statutes, ordinances and regulations regarding use of the WelfareAro website.
+
+                        <br><br>
+
+                        Membership is non-transferable and non refundable. Additionally, users agree to abide by any and all licensing rules or restrictions that have been created by the owners of the works contained within the WelfareAro website. Violating our Terms Of Use, Copyrights, or any of our licensing restrictions, will result in termination of membership, and forfeiture of all reseller licenses, without notice or refund. Furthermore, Use of this website constitutes acceptance of the Legal Agreement and Terms Of Use.
+
+                        <br><br>
+
+                        Warranties:
+
+                        <br><br>
+
+                        WelfareAro makes NO WARRANTEES express or implied and specifically does not warrant for donations.
+                        Communications from WelfareAro:
+                        By joining, or utilizing the WelfareAro website, you give us permission to contact you periodically via email with regards to your membership, or any other reason. If you wish to no longer receive communications from us, you will need to cancel your membership, or communication with us. If any provision of this agreement is void or unenforceable in whole or in part, the remaining provisions of this Agreement shall not be affected thereby. WelfareAro reserves the right to amend this agreement at any time.
+                        WelfareAro reserves the right to Amend these “Terms and Conditions” at any time.
+
+                        <br><br>
+                        Copyright WelfareAro
+
+
+                    </p>
+
+                </div>
+            </div>
+            <div class="col-lg-4 col-xl-5">
+                <div class="about_thumb">
+                    <!-- <img class="img-fluid w100" src="images/about/1.jpg" alt="1.jpg"> -->
+
+                </div>
+            </div>
+
+        </div>
+
+
+
+    </div>
+</section>
